8a7a863dffabf3461115a6bc48e163d0e8bb87bb
HamSeungChan <head0618@naver.com>
2023-03-02 10:59:15 +0900

    6단계

--- src/main/java/com/ll/App6.java
+++ src/main/java/com/ll/App6.java
@@ -5,7 +5,7 @@ import java.util.Collections;
 import java.util.List;
 import java.util.Scanner;
 
-public class App5 {
+public class App6 {
 
     private String command;
 
@@ -18,14 +18,14 @@ public class App5 {
 
         while (true) {
             System.out.print("명령) ");
-            command = sc.next();
+            command = sc.nextLine();
             if (command.equals("등록")) {
 
                 System.out.print("멍언 : ");
-                String wiseSaying = sc.next();
+                String wiseSaying = sc.nextLine();
 
                 System.out.print("작가 : ");
-                String author = sc.next();
+                String author = sc.nextLine();
 
                 storage.add(new Storage(wiseSaying,author,number));
                 System.out.println(storage.size()+"번 명언이 등록되었습니다.");
@@ -34,11 +34,18 @@ public class App5 {
             else if(command.equals("목록")){
                 System.out.println("번호 / 작가 / 명언");
                 System.out.println("----------------------");
-                Collections.reverse(storage);
-                for(Storage s : storage){
+                for(int i=storage.size()-1;i>=0;i--){
+                    Storage s = storage.get(i);
                     System.out.printf("%d / %s / %s\n",s.number,s.author,s.wiseSaying);
                 }
             }
+            else if(command.contains("?")){
+
+                int id = command.charAt(command.length()-1)-'0';
+                storage.remove(id-1);
+                System.out.println(id+"번 명언이 삭제되었습니다.");
+                }
+
             else {
                 break;
             }
@@ -46,14 +53,14 @@ public class App5 {
     }
 }
 
-//class Storage{
-//    String wiseSaying;
-//    String author;
-//    int number;
-//
-//    public Storage(String wiseSaying, String author,int number) {
-//        this.wiseSaying = wiseSaying;
-//        this.author = author;
-//        this.number = number;
-//    }
-//}
+class Storage{
+    String wiseSaying;
+    String author;
+    int number;
+
+    public Storage(String wiseSaying, String author,int number) {
+        this.wiseSaying = wiseSaying;
+        this.author = author;
+        this.number = number;
+    }
+}
